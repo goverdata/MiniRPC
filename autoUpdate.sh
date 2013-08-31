a5c20eecf1d7386c55cc2a3d933b5abbe915d647
zangxiaoqiang <399487232@qq.com>
2013-08-31 15:47:07 +0800

    Update autoUpdate.sh

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -1,13 +1,15 @@
 #/bin/bash
+username=""
+password=""
 git add src/*
 git add pom.xml
 git commit -m "rpc"
 expect -c "set timeout -1;
     spawn git push origin master;
     expect {
-      *sername:* {send -- "zangxiaoqiang"\r;
+      *sername:* {send -- $username\r;
         expect {
-        	*assword* {send -- "Pass8223152"\r;
+        	*assword* {send -- $password\r;
         		expect {
             		*denied* {exit 2;}
             		eof
